ce14355c986bfd589b1042301a592d7c068d7f81
Masoud <mmtaee64@gmail.com>
2026-02-13 13:07:37 +0330

    web: fix datepicker lang for issue: #118

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -6,12 +6,25 @@ import { BlueTheme } from '@/theme/LightTheme';
 import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n';
 import i18n from '@/plugins/i18n';
 import { useI18n } from 'vue-i18n';
+import DateFnsAdapter from '@date-io/date-fns'; // Import the adapter class
+import { enUS } from 'date-fns/locale';
+
 
 export default createVuetify({
     components,
     directives,
     locale: {
         adapter: createVueI18nAdapter({ i18n: i18n as any, useI18n })
+    },
+    date: {
+        adapter: DateFnsAdapter,
+        locale: {
+            en: enUS,
+            fa: enUS, // Persian
+            ar: enUS, // Arabic
+            he: enUS, // Hebrew
+            ur: enUS, // Urdu
+        },
     },
     theme: {
         defaultTheme: 'BlueTheme',
